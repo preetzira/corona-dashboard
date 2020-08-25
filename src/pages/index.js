a6921fb50310b69adbc2abd5465f9696254c3544
Gurpreet Singh <preetzira08@gmail.com>
2020-08-25 06:20:43 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -87,7 +87,7 @@ const IndexPage = () => {
           casesString = `${parseFloat(cases/1000).toFixed(2)}k+`
         }
         if ( cases > 10000 ) {
-          casesString = `${parseFloat(cases/10000).toFixed(1)}k+`
+          casesString = `${parseFloat(cases/1000).toFixed(1)}k+`
         }
         if ( cases > 1000000 ) {
           casesString = `${parseFloat(cases/1000000).toFixed(2)}M+`
